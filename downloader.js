81cc7ef7e7e81540444ff8575b5f234247ce124d
NickSpringall <123866019+NickSpringall@users.noreply.github.com>
2023-09-09 10:45:50 +1000

    feat url function done

--- downloader.js
+++ downloader.js
@@ -30,7 +30,28 @@ function getRandomPokemonId() {
 // Retriveve the image URL from that Pokemon data
 async function getRandomPokemonUrl(targetId = getRandomPokemonId()) {
 
+    // Retrieve the API data
+    let response = await fetch("https://pokeapi.co/api/vs/pokemon/" + targetId).catch(error => {
+        throw new Error("API failure.")
+    });
+
+    if (response.status == "404"){
+        throw new Error("API did not have data for the requested ID")
+    }
+
+    // Convert the response into usable JSON
+    let data = await response.json().catch(error => {
+        throw new Error("API did not return valid JSON.")
+    })
+
+    // Not optimised, it makes unnecessary variables
+    // let imageUrl = data.sprite.other["official-artwork"].front_default
+    // return imageUrl;
+
+    // more optimised, no extra junk variables
+    return data.sprite.other["official-artwork"].front_default
 }
+
 
 // Download that image and save it to the computer
 // Return the downloaded image's file path
